6bb56f283321cc452798ba61c7ac1091f5e60ba3
rishiraj007 <rishiraj@mounttalent.com>
2022-12-29 23:45:46 +0530

    [Prabhu] Add: Buy Stock

--- JSONProblem/Program.cs
+++ JSONProblem/Program.cs
@@ -18,21 +18,26 @@ namespace JSONProblem
             while (flag)
             {
                 Console.WriteLine("\nStock Management");
-                Console.WriteLine("\nSelect the option \n1.Stock Data Management \n2.Customer Data Management \n3.Exit");
+                Console.WriteLine("\nSelect the option \n1.Stock Data Management \n2.Customer Data Management \n3.Buy Stock \n4.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
                 {
-                    int choice = Convert.ToInt32(Console.ReadLine());
-                    switch (choice)
-                    {
-                        case 1:
-                            stockManage.ReadStockJsonFile(StockfilePath);
-                            break;
-                        case 2:
-                            stockManage.ReadCustomerJsonFile(CustomerfilePath);
-                            break;
-                        default:
-                            flag = false;
-                            break;
-                    }
+                    case 1:
+                        stockManage.ReadStockJsonFile(StockfilePath);
+                        break;
+                    case 2:
+                        stockManage.ReadCustomerJsonFile(CustomerfilePath);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter name of the stock u want to buy");
+                        string name=Console.ReadLine();
+                        stockManage.BuyStock(name);
+                        stockManage.WriteToStockJsonFile(StockfilePath);
+                        stockManage.WriteToCustomerJsonFile(CustomerfilePath);
+                        break;
+                    default:
+                        flag = false;
+                        break;
                 }
             }
         }
--- JSONProblem/StockManagement/StockManage.cs
+++ JSONProblem/StockManagement/StockManage.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace JSONProblem.StockManagement
 {
-    internal class StockManage
+    public class StockManage
     {
         List<Stock> stock = new List<Stock>(); 
         List<Stock> customer = new List<Stock>(); 
-        public void ReadStockJsonFile(string file)
+        public void ReadStockJsonFile(string filePath)
         {
-            var json=File.ReadAllText(file);
+            var json=File.ReadAllText(filePath);
             this.stock = JsonConvert.DeserializeObject<List<Stock>>(json);
             foreach(var content in stock)
             {
@@ -29,6 +29,57 @@ namespace JSONProblem.StockManagement
             {
                 Console.WriteLine("Stock Name: " + content.StockName + "\nStock Price: " + content.StockPrice + "\nNo of Shares: " + content.NoOfShares);
             }
+        }
+
+        public void BuyStock(string name)
+        {
+            double amount = 1000, count = 0;
+            Stock stocks;
+            foreach(var data in stock)
+            {
+                if(data.StockName.Equals(name))
+                {
+                    Console.WriteLine("Enter the no. of stocks u want to buy");
+                    int noOfStocks = Convert.ToInt32(Console.ReadLine());
+                    if(noOfStocks*data.StockPrice<=amount && noOfStocks<=data.NoOfShares)
+                    {
+                        Stock stock = new Stock()
+                        {
+                            StockName = data.StockName,
+                            StockPrice = data.StockPrice,
+                            NoOfShares = noOfStocks
+                        };
+                        data.NoOfShares = noOfStocks;
+                        amount-=data.StockPrice*noOfStocks;
+                        
+                        foreach(var account in customer)
+                        {
+                            if (account.StockName.Equals(name))
+                            {
+                                count++;
+                            }
+                        }
+                        if(count ==1)
+                        {
+                            data.NoOfShares += noOfStocks;
+                        }
+                        else 
+                        {
+                            customer.Add(stock);
+                        }
+                    }
+                }
+            }
+        }
+        public void WriteToStockJsonFile(string filePath)
+        {
+            var json = JsonConvert.SerializeObject(stock);
+            File.WriteAllText(filePath, json);
+        }
+        public void WriteToCustomerJsonFile(string filePath)
+        {
+            var json = JsonConvert.SerializeObject(stock);
+            File.WriteAllText(filePath, json);
         }
     }
 }
